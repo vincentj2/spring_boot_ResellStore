1e86eba091e01548aab89e0fe5d32f48b4d52b99
vincentj2 <vincentj@naver.com>
2020-12-20 19:32:02 +0900

    test

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -28,6 +28,7 @@ public class MemberRepositoryTest {
             //then
             Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
             Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
     }
     
 }
