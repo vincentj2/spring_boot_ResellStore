8334049e028f785f81b530af71cdce955702a5f5
vincentj2 <vincentj@naver.com>
2020-12-26 21:12:45 +0900

    create Item Service in Item domain

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //비지니스 로직 추가//
+    //도메인 주도 개발 방식 , StockQuantity를 관리하는 쪽에 비지니스 로직을 추가하는것이 더 객체지향스럽다
+
+    //stock 증가
+    public void addStock(int quantity){
+        this.StockQuantity += quantity;
+    }
+
+    //stock 감소
+    public void removeStock(int quantity){
+        int restStock = this.StockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.StockQuantity = restStock;
+    }
 }
