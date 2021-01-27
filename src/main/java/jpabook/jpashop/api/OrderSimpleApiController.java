49250519d1a5155fc600f19e9d3929c8db4a1946
vincentj2 <vincentj@naver.com>
2021-01-27 14:03:50 +0900

    lazy loadind api optimize V4

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,15 +71,25 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
         return result;
     }
+    /**
+     * V4. JPA에서 DTO로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하는 데이터만 선택해서 조회
+     */
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4(){
+        return orderSimpleQueryRepository.findOrdersDtos();
+    }
 
     @Data
-    static class SimpleOrderDto{
+    static class SimpleOrderDto {
         private Long orderId;
         private String name;
-        private LocalDateTime orderDate;
+        private LocalDateTime orderDate; //주문시간
         private OrderStatus orderStatus;
         private Address address;
-
         public SimpleOrderDto(Order order) {
             orderId = order.getId();
             name = order.getMember().getName();
@@ -86,4 +98,5 @@ public class OrderSimpleApiController {
             address = order.getDelivery().getAddress();
         }
     }
+    
 }
